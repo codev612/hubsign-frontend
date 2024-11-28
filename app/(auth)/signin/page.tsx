1c92377b4eb03c7a062d22ba8f68046ed6b151e4
CoDeveloper612 <michaeljoe612@outlook.com>
2024-11-28 03:08:38 -0800

    initial commit

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,103 @@
+'use client'
+
+import React, { useState } from "react";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+import { Input } from "@nextui-org/input";
+import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
+import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
+import { Button } from "@nextui-org/button";
+
+export default function Signin() {
+
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
+
+    return (
+        <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <form className="flex flex-col items-center justify-center bg-forecolor p-10 gap-4 rounded-md">
+                <h1>Log in to eSign</h1>
+                <p className="text-text mb-2">Enter your credentials to access your account</p>
+                <Input 
+                type="email" 
+                label="Email" 
+                variant={'bordered'} 
+                labelPlacement={'outside'} 
+                placeholder="Enter your email" 
+                size="md"
+                />
+                <Input
+                label="Password"
+                variant="bordered"
+                placeholder="Enter your password"
+                labelPlacement={'outside'}
+                size="md"
+                endContent={
+                    <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                    {isVisible ? (
+                        <VisibilityOffOutlinedIcon className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                        <VisibilityOutlinedIcon className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                    </button>
+                }
+                type={isVisible ? "text" : "password"}
+                description={<Link href="/"><p className="text-text">Forgot your password?</p></Link>}
+                />
+                <Button color="primary" fullWidth className="text-white" size="md">Log in</Button>
+                <div className="flex flex-col items-center justify-center">
+                    <Link href="/"><p className="text-text">Don't have an account?</p></Link>
+                    <Link href="/"><p>Signup for 30 days free trial</p></Link>
+                </div>
+            </form>
+        {/* <div className="inline-block max-w-xl text-center justify-center bg-forecolor">
+            <span className={title()}>Make&nbsp;</span>
+            <span className={title({ color: "violet" })}>Signin&nbsp;</span>
+            <br />
+            <span className={title()}>
+            websites regardless of your design experience.
+            </span>
+            <div className={subtitle({ class: "mt-4" })}>
+            Beautiful, fast and modern React UI library.
+            </div>
+        </div>
+
+        <div className="flex gap-3">
+            <Link
+            isExternal
+            className={buttonStyles({
+                color: "primary",
+                radius: "full",
+                variant: "shadow",
+            })}
+            href={siteConfig.links.docs}
+            >
+            Documentation
+            </Link>
+            <Link
+            isExternal
+            className={buttonStyles({ variant: "bordered", radius: "full" })}
+            href={siteConfig.links.github}
+            >
+            <GithubIcon size={20} />
+            GitHub
+            </Link>
+        </div>
+
+        <div className="mt-8">
+            <Snippet hideCopyButton hideSymbol variant="bordered">
+            <span>
+                Get started by editing <Code color="primary">app/page.tsx</Code>
+            </span>
+            </Snippet>
+        </div> */}
+        </section>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+import { nextui } from "@nextui-org/theme";
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+        mono: ["var(--font-mono)"],
+      },
+    },
+  },
+  darkMode: "class",
+  darkMode: "class",
+  plugins: [
+    nextui({
+      layout: {
+        disabledOpacity: "0.3", // opacity-[0.3]
+        fontSize: {
+          tiny: "0.75rem", // text-tiny
+          small: "0.875rem", // text-small
+          medium: "1rem", // text-medium
+          large: "1.125rem", // text-large
+        },
+        radius: {
+          small: "2px", // rounded-small
+          medium: "8px", // rounded-medium
+          large: "6px", // rounded-large
+        },
+        borderWidth: {
+          small: "1px", // border-small
+          medium: "1px", // border-medium
+          large: "2px", // border-large
+        },
+      },
+      themes: {
+        light: {
+          colors: {
+            background: "#E6E6E6",
+            forecolor: "#F4F4F4",
+            text: "#525252",
+            primary: "#2563EB",
+          },
+        },
+        dark: {},
+      },
+    }),
+  ],
+};
